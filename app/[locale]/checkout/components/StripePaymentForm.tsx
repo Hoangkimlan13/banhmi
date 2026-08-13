188b1acb380b2c53f2271dd9064667c99eae8bcf
Hoangkimlan13 <hoangkimlan13@gmail.com>
2026-08-13 23:17:48 +0900

    fix: update checkout and prisma configuration

--- app/[locale]/checkout/components/StripePaymentForm.tsx
+++ app/[locale]/checkout/components/StripePaymentForm.tsx
@@ -319,7 +319,7 @@ export default function StripePaymentForm({
             options={{
               layout: {
                 type: 'accordion',
-                defaultCollapsed: false,
+                defaultCollapsed: true,
               },
             }}
           />
